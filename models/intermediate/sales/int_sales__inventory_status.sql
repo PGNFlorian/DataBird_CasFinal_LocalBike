937cd7a68d589c30a1e33f6289e93bc7bad6df47
Florian PIGEON <fpigeon@vivalto-sante.com>
2025-11-03 16:59:51 +0000

    FIX-null timesatmp value

--- models/intermediate/sales/int_sales__inventory_status.sql
+++ models/intermediate/sales/int_sales__inventory_status.sql
@@ -1,5 +1,5 @@
 SELECT
-  s.store_id,
+  st.store_id,
   st.store_name,
   p.product_id,
   p.product_name,
@@ -7,7 +7,7 @@ SELECT
   b.brand_name,
   stq.quantity AS stock_quantity
 FROM {{ ref('stg_production__stocks') }} stq
-LEFT JOIN {{ ref('stg_sales__stores') }} st USING (store_id)
-LEFT JOIN {{ ref('stg_production__products') }} p USING (product_id)
-LEFT JOIN {{ ref('stg_production__categories') }} c USING (category_id)
-LEFT JOIN {{ ref('stg_production__brands') }} b USING (brand_id)
+LEFT JOIN {{ ref('stg_sales__stores') }} st ON st.store_id = stq.store_id
+LEFT JOIN {{ ref('stg_production__products') }} p ON p.product_id = stq.product_id
+LEFT JOIN {{ ref('stg_production__categories') }} c ON c.category_id = p.category_id
+LEFT JOIN {{ ref('stg_production__brands') }} b ON b.brand_id = p.brand_id
--- models/intermediate/sales/int_sales__orders.sql
+++ models/intermediate/sales/int_sales__orders.sql
@@ -10,7 +10,10 @@ SELECT
   st.first_name || ' ' || st.last_name AS staff_name,
   o.customer_id,
   c.first_name || ' ' || c.last_name AS customer_name
-FROM {{ ref('stg_sales__orders') }} o
-LEFT JOIN {{ ref('stg_sales__customers') }} c USING (customer_id)
-LEFT JOIN {{ ref('stg_sales__staffs') }} st USING (staff_id)
-LEFT JOIN {{ ref('stg_sales__stores') }} s USING (store_id)
+FROM {{ ref('stg_sales__orders') }} AS o
+LEFT JOIN {{ ref('stg_sales__customers') }} AS c
+  ON o.customer_id = c.customer_id
+LEFT JOIN {{ ref('stg_sales__staffs') }} AS st
+  ON o.staff_id = st.staff_id
+LEFT JOIN {{ ref('stg_sales__stores') }} AS s
+  ON o.store_id = s.store_id
