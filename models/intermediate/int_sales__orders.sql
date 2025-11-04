be1cdb90c31180119de32f3d6c1be90e96173d83
Florian PIGEON <fpigeon@vivalto-sante.com>
2025-11-04 15:08:41 +0000

    FIX-Intermediates models with test and FEAT mart models

--- models/intermediate/int_sales__orders.sql
+++ models/intermediate/int_sales__orders.sql
@@ -1,4 +1,15 @@
-WITH orders AS (
+WITH order_items AS (
+    SELECT
+        order_id,
+        item_id,
+        product_id,
+        quantity,
+        list_price,
+        discount
+    FROM {{ ref('stg_sales__order_items') }}
+),
+
+orders AS (
     SELECT
         order_id,
         customer_id,
@@ -14,7 +25,9 @@ WITH orders AS (
 customers AS (
     SELECT
         customer_id,
-        first_name || ' ' || last_name AS customer_name
+        first_name || ' ' || last_name AS customer_name,
+        city as customer_city,
+        state as customer_state
     FROM {{ ref('stg_sales__customers') }}
 ),
 
@@ -33,18 +46,32 @@ stores AS (
 )
 
 SELECT
+    o.order_id || '|' || oi.item_id as order_item_id,
+    o.store_id || '|' || oi.product_id as store_product_id,
+
     o.order_id,
     o.order_date,
     o.required_date,
     o.shipped_date,
     o.order_status,
+    
+    oi.item_id,
+    oi.quantity,
+    oi.list_price,
+    oi.discount,
+    ROUND(oi.quantity * oi.list_price * (1 - oi.discount), 2) AS total,
+
     o.store_id,
     s.store_name,
     o.staff_id,
     st.staff_name,
     o.customer_id,
-    c.customer_name
-FROM orders o
+    c.customer_name,
+    c.customer_city,
+    c.customer_state
+FROM order_items oi
+LEFT JOIN orders o
+    ON o.order_id = oi.order_id
 LEFT JOIN customers c
     ON o.customer_id = c.customer_id
 LEFT JOIN staffs st
